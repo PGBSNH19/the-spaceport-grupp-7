95c794501440a4e203da2bc949727a02d862bb97
Andreas Brochs <55091646+AndreasBrochs@users.noreply.github.com>
2020-03-23 15:57:23 +0100

    @andreasbrochs

---
+++ Source/TheSpacePort/Swreponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheSpacePort
+{
+    internal class SwAPIreponse
+    {
+
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public IEnumerable<Person> Results { get; set; }
+    }
+}
